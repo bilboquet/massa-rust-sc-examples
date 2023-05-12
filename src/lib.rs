1a4e05eb9ac46d80d1c0ef790a9525ba4f72af9a
Jean-François Morcillo <jfm@massa.net>
2023-05-12 17:15:44 +0200

    Fix test after moving sdk into its own repo
    Signed-off-by: Jean-François Morcillo <jfm@massa.net>

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,23 @@
 // ****************************************************************************
+// needed to override the panic handler
 #![no_std]
 // ****************************************************************************
 
+// ****************************************************************************
+// Imports from massa-rust-sc-sdk
+// ****************************************************************************
 use massa_rust_sc_sdk as sdk;
-use sdk::{
-    abis::{echo::echo, log::log},
-    *,
-};
+// as we are in a no_std environment, the sdk redifined the allocators for
+// default containers, use 'use sdk::*' to get them
+use sdk::*;
+// Imports what is needed from the SDK
+use sdk::abis::{echo, log};
+// ****************************************************************************
 
+// ****************************************************************************
+// SC can exposte a main function, but it is not required and it must be
+// preceded by #[cfg(not(test))] to avoid conflicts with the test runner
+// ****************************************************************************
 #[cfg(not(test))]
 #[no_mangle]
 pub extern "C" fn main(_arg: i32) -> i32 {
@@ -24,13 +34,8 @@ pub fn call_echo(arg_ptr: u32) -> u32 {
     let arg = get_parameters(arg_ptr);
     log("call_echo".to_string());
 
-    // assert_eq!(arg.len(), 4);
-    log(format!("arg len: {}", arg.len()));
-
-    // assert_eq!(arg, "test".to_string().into_bytes());
-    let arg = "test".to_string().into_bytes();
-    let ret = echo(arg);
-    log(format!("ret len: {}", ret.len()));
+    let ret = echo(arg.clone());
+    assert_eq!(ret, arg);
 
     // data MUST be returned this way
     encode_length_prefixed(ret)
@@ -38,22 +43,28 @@ pub fn call_echo(arg_ptr: u32) -> u32 {
 
 #[cfg(test)]
 mod tests {
+    // *************************************************************************
+    // Imports from the std crate to return to std environment and write tests
+    // more simply
+    // *************************************************************************
     extern crate std;
+    use massa_rust_sc_sdk as sdk;
+    use sdk::{test, *};
+
     use crate::call_echo;
-    use crate::sdk::{self, *};
 
     #[test]
     fn test_call_echo() {
         let test_msg = "test".to_string().into_bytes();
 
         // simulate arguments passing from the host to the SC
-        let buf_ptr = sdk::test::host_write_buffer(&test_msg);
+        let buf_ptr = test::host_write_buffer(&test_msg);
 
         // call the SC function and get the result
         let result_ptr = call_echo(buf_ptr);
 
         // simulate reading the result from the SC
-        let result = sdk::test::host_read_buffer(result_ptr);
+        let result = test::host_read_buffer(result_ptr);
 
         // decode the result from the SC
         let result = String::from_utf8_lossy(&result);
